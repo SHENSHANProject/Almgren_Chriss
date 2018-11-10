a8100f337356027c574dabaf3552daf1eb424443
ShanCheng1994 <32513190+ShanCheng1994@users.noreply.github.com>
2018-11-10 21:38:29 +0100

    Add files via upload

--- Vol_est/data_gen.py
+++ Vol_est/data_gen.py
@@ -23,20 +23,26 @@ import matplotlib.pyplot as plt
 # Parameter for noise
 #var_e = 0.0005**2 # variance of noise
 
-def Xt(M,x0,miu = 0.05,k = 5,alpha = 0.04,gamma = 0.5,rho =-0.5,T = 1.0/252,N = int(6.5*3600)+1):
+def Xt(x0,M=1,miu = 0.05,k = 5,alpha = 0.04,gamma = 0.5,rho =-0.5,T = 1.0/252,N = int(6.5*3600)+1):
     t = T/N
     x = np.zeros((M,N))
-    
+    sigma4 = np.zeros((M,))
+    sigma2 = np.zeros((M,))
     
     for j in range(M):
         vi = 0.05
         x[j,0]=x0
+        dsigma4 = 0# for calclating the integral of sigma 4
+        dsigma2 = 0 #for calclating the integral of sigma 2
         for i in range(1,N):
             dB,dW = npr.multivariate_normal([0,0],[[t,rho*t],[rho*t,t]])
-            x[j,i] = x[j,i-1]+(miu-vi/2)*t+vi**2*dB
+            x[j,i] = x[j,i-1]+(miu-vi/2)*t+np.sqrt(vi)*dB
             vi = vi+k*(alpha-vi)*t+gamma*np.sqrt(vi)*dW
-    
-    return x
+            dsigma4 += vi**2*t
+            dsigma2 += vi*t
+        sigma4[j] = dsigma4
+        sigma2[j] = dsigma2
+    return x,sigma4,sigma2
 
 
 #x = Xt(1,np.log(20))
@@ -45,8 +51,7 @@ def Xt(M,x0,miu = 0.05,k = 5,alpha = 0.04,gamma = 0.5,rho =-0.5,T = 1.0/252,N = 
 #
 #plt.plot(n,s.T)
 
-def Yt(xt,var_e = 0.0005**2):
-    M = xt.shape[0]
+def Yt(M,xt,var_e = 0.0005**2):
     N = xt.shape[1]
     epsilon = np.random.randn(M,N)*np.sqrt(var_e)
     return xt+epsilon
